62c3f3ea2455904b1d78520c4d439e47fbd27b78
henrique silverio <henrique.silverio@db1.com.br>
2019-10-11 17:52:02 -0300

    melhorias no ranking

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,19 +1,97 @@
 import React from 'react';
-import { StyleSheet, View, SafeAreaView, ScrollView } from 'react-native';
-import { ListItem, Icon } from 'react-native-elements'
+import {
+  View,
+  TouchableOpacity,
+  Animated,
+  Alert,
+  StyleSheet,
+  SafeAreaView,
+  ScrollView
+} from 'react-native';
+import { ListItem } from 'react-native-elements';
+import { Swipeable } from 'react-native-gesture-handler';
 
 const list = [
   {
+    id: 1,
     name: 'Amy Farha',
     avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
     subtitle: 'Vice President'
   },
   {
+    id: 2,
     name: 'Chris Jackson 1',
     avatar_url: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
     subtitle: 'Vice Chairman'
   },
 ];
+
+const onPressFromLeft = (player) => {
+  Alert.alert(
+    'Rebuy',
+    `Confirma o rebuy do ${player.name}?`,
+    [
+      {
+        text: 'Cancelar',
+        onPress: () => console.log('Cancel Pressed'),
+        style: 'cancel',
+      },
+      {
+        text: 'Confirmar', onPress: () => console.log('OK Pressed')
+      },
+    ]
+  );
+}
+
+const onPressFromRight = (player) => {
+  Alert.alert('Eliminação', `Confirma a eliminação do ${player.name} na posição ${list.length}?.`);
+}
+
+const LeftActions = ({ progress, dragX, player }) => {
+  const scale = dragX.interpolate({
+    inputRange: [0, 100],
+    outputRange: [0, 1],
+    extrapolate: 'clamp'
+  });
+
+  return (
+    <TouchableOpacity onPress={() => onPressFromLeft(player)}>
+      <View style={styles.leftAction}>
+        <Animated.Text
+          style={[styles.actionText,
+          {
+            transform: [{ scale }]
+          }
+          ]}>
+          Rebuy
+        </Animated.Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
+
+const RightActions = ({ progress, dragX, player }) => {
+  const scale = dragX.interpolate({
+    inputRange: [-100, 0],
+    outputRange: [1, 0],
+    extrapolate: 'clamp'
+  });
+
+  return (
+    <TouchableOpacity onPress={() => onPressFromRight(player)}>
+      <View style={styles.rightAction}>
+        <Animated.Text
+          style={[styles.actionText,
+          {
+            transform: [{ scale }]
+          }
+          ]}>
+          Eliminar
+        </Animated.Text>
+      </View>
+    </TouchableOpacity>
+  )
+}
 
 const Ranking = ({ navigation }) => (
   <SafeAreaView style={styles.container}>
@@ -21,27 +99,26 @@ const Ranking = ({ navigation }) => (
       <View>
         {
           list.map((item, i) => (
-            <ListItem
+            <Swipeable
               key={i}
-              title={item.name}
-              subtitle={item.subtitle}
-              leftAvatar={{ source: { uri: item.avatar_url } }}
-              bottomDivider
-            />
+              renderLeftActions={(progress, dragX) =>
+                <LeftActions progress={progress} dragX={dragX} player={item} />
+              }
+              renderRightActions={(progress, dragX) =>
+                <RightActions progress={progress} dragX={dragX} player={item} />
+              }
+            >
+              <ListItem
+                title={item.name}
+                subtitle={item.subtitle}
+                leftAvatar={{ source: { uri: item.avatar_url } }}
+                bottomDivider
+              />
+            </Swipeable>
           ))
         }
       </View>
     </ScrollView>
-    <Icon reverse
-      reverseColor='#212121'
-      name='add'
-      type='material'
-      size={30}
-      color='#ffc107'
-      containerStyle={styles.icon}
-      iconStyle={{ fontSize: 40 }}
-      onPress={()=> navigation.navigate('Position')}
-    />
   </SafeAreaView>
 );
 
@@ -63,10 +140,22 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#263238'
   },
-  icon: {
-    position: 'absolute',
-    zIndex: 100,
-    bottom: 0,
-    right: 0
+  leftAction: {
+    backgroundColor: '#4caf50',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    padding: 3.5
+  },
+  rightAction: {
+    backgroundColor: '#f44336',
+    alignItems: 'flex-end',
+    justifyContent: 'center',
+    padding: 3.5
+  },
+  actionText: {
+    color: '#fff',
+    fontWeight: '600',
+    padding: 20,
+    fontSize: 20
   }
 });
